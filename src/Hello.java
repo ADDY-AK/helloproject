e0e9ded602652e8be03ecd5a4cbd7013ed623405
ADDY-AK <adkgta2000@gmail.com>
2020-02-23 12:56:39 +0530

    add new line in project

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("welcome");
         System.out.println("this is the demo project");
+        System.out.println("hjhjhj");
     }
 }
